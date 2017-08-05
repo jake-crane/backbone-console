31d73788bb942c2e009268fe7ff8be6e741aa317
Jake Crane <jake@jakecrane.com>
2017-08-05 14:48:50 -0500

    Use view wrapper element for each configuration

--- src/js/configuration-collection/configuration-collection.view.js
+++ src/js/configuration-collection/configuration-collection.view.js
@@ -2,14 +2,13 @@ window.app = window.app || {};
 
 app.ConfigurationCollectionView = Backbone.View.extend({
   template: _.template($('#configuration-collection-template').html()),
-  tagName: 'div',
   el: $('#configuration-collection-container'),
 
   initialize: function () {
     this.model.on('add', this.render, this);
     this.model.on('remove', this.render, this);
   },
-  render: function () {;
+  render: function () {
     this.$el.html(this.template(this.model.models));
     return this;
   }
--- src/js/configuration/configuration.view.js
+++ src/js/configuration/configuration.view.js
@@ -1,6 +1,8 @@
 window.app = window.app || {};
 
 app.ConfigurationView = Backbone.View.extend({
+  tagName: 'tr',
+  className: 'configuration-view',
   template: _.template($('#configuration-template').html()),
 
   initialize: function () {
