73290fbbb55713e42a5cce4a1088cb4c26e19518
Jake Crane <jake@jakecrane.com>
2017-08-05 20:22:49 -0500

    Allow configuration edits and saves

--- src/js/configuration-collection/configuration-collection.view.js
+++ src/js/configuration-collection/configuration-collection.view.js
@@ -7,12 +7,16 @@ app.ConfigurationCollectionView = Backbone.View.extend({
   initialize: function () {
     this.model.on('add', this.render, this);
     this.model.on('remove', this.render, this);
+    this.model.on('change', this.render, this);
   },
   render: function () {
     this.$el.html(this.template());
     var self = this;
     _.each(this.model.models, function (configuration) {
-      var $config = new app.ConfigurationView({model: configuration }).render().$el;
+      var view = configuration.attributes.editMode ? app.EditConfigurationView : app.ConfigurationView;
+      var $config = new view({
+        model: configuration
+      }).render().$el;
       self.$el.find('tbody').append($config);
     });
     return this;
--- src/js/configuration/configuration.model.js
+++ src/js/configuration/configuration.model.js
@@ -13,12 +13,29 @@ app.Configuration = Backbone.Model.extend({
     };
   },
   edit: function () {
-    this.set('editMode', true);
+    this.set({
+      editMode: true
+    });
   },
   cancelEdit: function () {
-    this.set('editMode', false);
+    this.set({
+      editMode: false
+    });
+  },
+  saveConfig: function (updatedConfiguration) {
+    this.save(updatedConfiguration);
   },
   delete: function () {
     this.destroy();
+  },
+  toJSON: function () {
+    return {
+      name: this.attributes.name,
+      key: this.attributes.key,
+      value: this.attributes.value,
+      description: this.attributes.description,
+      type: this.attributes.type,
+      id: this.attributes.id
+    };
   }
 });
