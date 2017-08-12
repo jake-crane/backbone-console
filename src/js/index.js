544e74581af283925f8710f7420a051f2a542cd3
Jake Crane <jake@jakecrane.com>
2017-08-12 16:07:54 -0500

    Wait for document to load before starting app

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-app.configurationCollection = new app.ConfigurationCollection();
-app.configurationCollectionView = new app.ConfigurationCollectionView({
-    collection: app.configurationCollection
+$(function () {
+    app.configurationCollection = new app.ConfigurationCollection();
+    app.configurationCollectionView = new app.ConfigurationCollectionView({
+        collection: app.configurationCollection
+    });
 });
