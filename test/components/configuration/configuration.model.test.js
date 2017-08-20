b3c10a3476c6e5d33e4ccff555be0328faab2d7d
Jake Crane <jake@jakecrane.com>
2017-08-20 17:00:32 -0500

    Add a unit test

---
+++ test/components/configuration/configuration.model.test.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+var Configuration = require('../../../src/components/configuration/configuration.model');
+
+describe('Configuration model test', function () {
+	it('should ignore extra attributes', function () {
+		var config = new Configuration({
+			name: 'config name',
+			key: 'config key',
+			value: 'config value',
+			description: 'config description',
+			type: 'XML',
+			editMode: true
+		});
+		var json = JSON.stringify(config);
+		expect(json).to.equal('{"name":"config name","key":"config key","value":"config value","description":"config description","type":"XML"}');
+	});
+});
