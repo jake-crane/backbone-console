f745453b836c591de71ebb01f2a8e603ef929f0e
Jake Crane <jacrane@dstsystems.com>
2017-09-27 12:55:18 -0500

    Start adding new config modal

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,8 @@ import '../components/search/custom.search';
 
 $('#configuration-collection-container').configCollection();
 $('#search').search();
+
+var dialog = document.querySelector('dialog');
+$('#add-button').on('click', () => {
+	dialog.showModal();
+});
