5e7b2aea7ef9c4d91f3001a94eda6f447da7daad
Jake Crane <jake@jakecrane.com>
2017-10-07 14:38:59 -0500

    Add search

--- src/components/search/search.view.js
+++ src/components/search/search.view.js
@@ -1,8 +1,17 @@
 import Backbone from 'backbone';
+import template from 'lodash/template';
+import searchTemplate from './search.html';
+import './search.css';
 
 export default Backbone.View.extend({
 	events: {
 		'keyup': 'search',
+	},
+
+	template: template(searchTemplate),
+	render: function () {
+		this.$el.html(this.template());
+		return this;
 	},
 	search: function (e) {
 		Backbone.trigger('search', e.target.value);
