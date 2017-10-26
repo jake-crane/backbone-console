2aa3cc475baf6454b1cd175d76f0cf29a20909f7
Jake Crane <jake@jakecrane.com>
2017-10-25 20:08:03 -0500

    Nav changes

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,8 +2,14 @@ import $ from 'jquery';
 
 const $pages = $('.page');
 const $comspage = $('.coms-page');
+const $configpage = $('.config-page');
+
+$('.config-link').on('click', (e) => {
+	$pages.addClass('no-display');
+	$configpage.removeClass('no-display');
+});
 
 $('.coms-link').on('click', (e) => {
-	$pages.hide();
-	$comspage.show();
+	$pages.addClass('no-display');
+	$comspage.removeClass('no-display');
 });
