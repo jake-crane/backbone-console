5723ebcae12005ac5e989889f7beb93874bb0117
Jake Crane <jacrane@dstsystems.com>
2017-11-01 07:47:53 -0500

    Add custom sidenav

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,19 +3,25 @@ import $ from 'jquery';
 const $pages = $('.page');
 const $comspage = $('.coms-page');
 const $configpage = $('.config-page');
+const $configLinks = $('.config-link');
+const $comsLinks = $('.coms-link');
 
 let comsLoaded = false;
 
-$('.config-link').on('click', (e) => {
+$configLinks.on('click', (e) => {
 	$pages.addClass('no-display');
+	$comsLinks.parent().removeClass('selected');
 	$configpage.removeClass('no-display');
+	$configLinks.parent().addClass('selected');
 });
 
-$('.coms-link').on('click', (e) => {
+$comsLinks.on('click', (e) => {
 	if (!comsLoaded) {
 		$('#communications').communications();
 		comsLoaded = true;
 	}
 	$pages.addClass('no-display');
+	$configLinks.parent().removeClass('selected');
 	$comspage.removeClass('no-display');
+	$comsLinks.parent().addClass('selected');
 });
